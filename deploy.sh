0d30251a7881d98ccef2b5fcd8021821025dc472
vamaytsvetov <vamaytsvetov@avito.ru>
2024-02-04 14:37:55 +0700

    upd ip

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #! bin/bash
 
 env GOOS=linux GOARCH=amd64 go build -o colala .
-rsync -avz colala mausved@158.160.86.76:/home/mausved/colala/
+rsync -avz colala mausved@158.160.30.4:/home/mausved/colala/
 rm colala
